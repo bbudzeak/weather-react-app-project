66b07f964c3fc923f5a39c130e395431f51650db
bbudzeak <bzezulka@gmail.com>
2022-03-09 15:43:43 -0500

    Added link to github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">Weather App</div>
+      <div className="git-link">
+        <span> Project coded by Bethany Budzeak, </span>
+        <a
+          href="https://github.com/bbudzeak/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </div>
     </div>
   );
 }
