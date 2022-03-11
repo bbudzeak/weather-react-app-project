b485b9343fe210a34039d7883a949da35a9fbbe9
bbudzeak <bzezulka@gmail.com>
2022-03-11 12:57:29 -0500

    Added date functionality

--- src/Weather.js
+++ src/Weather.js
@@ -1,60 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
+const apiKey = "34b734b8fe98139b171f0fae0dc3bc5c";
+
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form className="mb-4">
-        <div className="row">
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form className="mb-4">
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-6">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-40"
+              />
+              <input
+                type="button"
+                className="btn btn-primary w-40"
+                value="Search Current Location"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-1">
           <div className="col-6">
-            <input
-              type="search"
-              placeholder="Type a city..."
-              className="form-control"
-              autoFocus="on"
-            />
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/snow_s_rain.png"
+                alt="snow"
+                className="float-start"
+              />
+              <span className="float-start">
+                <span className="temperature">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="unit">°C</span>
+              </span>
+            </div>
           </div>
-          <div className="col-6">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-40"
-            />
-            <input
-              type="button"
-              className="btn btn-primary w-40"
-              value="Search Current Location"
-            />
+          <div className="description col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 08:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row mt-1">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/snow_s_rain.png"
-              alt="snow"
-              className="float-start"
-            />
-            <span className="float-start">
-              <span className="temperature">6</span>
-              <span className="unit">°C</span>
-            </span>
-          </div>
-        </div>
-        <div className="description col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 50%</li>
-            <li>Wind: 15 km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "Chicago";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
