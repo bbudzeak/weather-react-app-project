08e123b1e2936ed7c6a6a0b84d7002c70fcccd01
bbudzeak <bzezulka@gmail.com>
2022-03-11 16:58:04 -0500

    Updated to secure link

--- src/Weather.js
+++ src/Weather.js
@@ -25,7 +25,7 @@ export default function Weather() {
   }
 
   function search() {
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
